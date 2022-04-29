5df2b8c239f81188b248a5b90495c3cbd27cefac
Kevin Sana <kevin.sana@outlook.com>
2022-04-29 14:42:22 -0500

    added new component to routes and updated navbar, created new page component for contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/navbar.component';
 import Background from './components/background/background.component';
 import HomePage from './pages/home/home.page';
+import ContactPage from './pages/contact/contact.page';
 
 import './app.styles.scss';
 
@@ -14,6 +15,7 @@ function App() {
       <Background />
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/contact' element={<ContactPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/contact/contact.page.jsx
@@ -0,0 +1,14 @@
+import './contact.styles.scss';
+
+
+const ContactPage = () => {
+    return (
+        <div className='contact-container'>
+            <div className='contact-box'>
+                <h1>Contact box</h1>
+            </div>
+        </div>
+    );
+};
+
+export default ContactPage;
