1924be83f7adafe274bb00afae9eb9ebafd3e896
Kevin Sana <kevin.sana@outlook.com>
2022-05-02 21:22:17 -0500

    added project card components and added styling

--- src/pages/projects/projects.component.jsx
+++ src/pages/projects/projects.component.jsx
@@ -1,9 +1,48 @@
 import './projects.styles.scss';
 
+const ProjectCard = ({ title, desc, demo, git, img }) => {
+    return (
+        <div className='card'>
+            <h1>{title}</h1>
+            <img src={img} alt={img} />
+            <p>{desc}</p>
+            <div className='card-links'>
+                <a href={demo} target='_blank' rel='noreferrer'>Demo</a>
+                <a href={git} target='_blank' rel='noreferrer'>GitHub</a>
+            </div>
+        </div>
+    );
+};
+
 const ProjectsPage = () => {
     return (
-        <div className='projects-container'>
-            <h1>Projects</h1>
+        <div className='projects-page-container'>
+            <div className='card-container'>
+                <ProjectCard
+                    title='Weather App'
+                    img='https://via.placeholder.com/250x200'
+                    desc='Application that uses the openweather API and displays
+                a 5 day forecast. Location is hardcoded to Winnipeg, MB.'
+                    demo='https://krownlesskev.github.io/weather-app/'
+                    git='https://github.com/krownlesskev/weather-app'
+                />
+                <ProjectCard
+                    title='Weather App'
+                    img='https://via.placeholder.com/250x200'
+                    desc='Application that uses the openweather API and displays
+                a 5 day forecast. Location is hardcoded to Winnipeg, MB.'
+                    demo='https://krownlesskev.github.io/weather-app/'
+                    git='https://github.com/krownlesskev/weather-app'
+                />
+                <ProjectCard
+                    title='Weather App'
+                    img='https://via.placeholder.com/250x200'
+                    desc='Application that uses the openweather API and displays
+                a 5 day forecast. Location is hardcoded to Winnipeg, MB.'
+                    demo='https://krownlesskev.github.io/weather-app/'
+                    git='https://github.com/krownlesskev/weather-app'
+                />
+            </div>
         </div>
     );
 };
