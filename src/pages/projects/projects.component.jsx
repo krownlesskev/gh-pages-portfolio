d5288ef824936337fd3daf749909cd8b7b8d0b24
Kevin Sana <kevin.sana@outlook.com>
2022-05-02 21:33:53 -0500

    added actual projects and fixed styling

--- src/pages/projects/projects.component.jsx
+++ src/pages/projects/projects.component.jsx
@@ -1,4 +1,7 @@
 import './projects.styles.scss';
+import image1 from '../../assets/hatchways.png';
+import image2 from '../../assets/chinup.png';
+import image3 from '../../assets/smartbrain.png';
 
 const ProjectCard = ({ title, desc, demo, git, img }) => {
     return (
@@ -20,27 +23,27 @@ const ProjectsPage = () => {
             <div className='card-container'>
                 <ProjectCard
                     title='Weather App'
-                    img='https://via.placeholder.com/250x200'
+                    img={image1}
                     desc='Application that uses the openweather API and displays
                 a 5 day forecast. Location is hardcoded to Winnipeg, MB.'
                     demo='https://krownlesskev.github.io/weather-app/'
                     git='https://github.com/krownlesskev/weather-app'
                 />
                 <ProjectCard
-                    title='Weather App'
-                    img='https://via.placeholder.com/250x200'
-                    desc='Application that uses the openweather API and displays
-                a 5 day forecast. Location is hardcoded to Winnipeg, MB.'
-                    demo='https://krownlesskev.github.io/weather-app/'
-                    git='https://github.com/krownlesskev/weather-app'
+                    title='Mental Health Awareness App'
+                    img={image2}
+                    desc='Generates random excuses and also has other resources(WIP)
+                     for mental health awareness.'
+                    demo='https://krownlesskev.github.io/chin-up/'
+                    git='https://github.com/krownlesskev/chin-up'
                 />
                 <ProjectCard
-                    title='Weather App'
-                    img='https://via.placeholder.com/250x200'
-                    desc='Application that uses the openweather API and displays
-                a 5 day forecast. Location is hardcoded to Winnipeg, MB.'
-                    demo='https://krownlesskev.github.io/weather-app/'
-                    git='https://github.com/krownlesskev/weather-app'
+                    title='Facial Recognition App'
+                    img={image3}
+                    desc='Full stack application that detects faces in uploaded
+                    images and draws a box around the face.'
+                    demo='https://krownless-smart-brain.herokuapp.com/'
+                    git='https://github.com/krownlesskev/FaceRecognitionBrain'
                 />
             </div>
         </div>
